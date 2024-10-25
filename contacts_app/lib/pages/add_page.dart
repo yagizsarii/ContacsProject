a82bdfb92a85406c1b3283df8147742ddbc4e85c
yagizsarii <yagiz.sari@archerobotics.com>
2024-10-26 00:36:10 +0300

    feat: add page added

--- contacts_app/lib/pages/add_page.dart
+++ contacts_app/lib/pages/add_page.dart
@@ -1,46 +1,56 @@
 import 'package:contacts_app/components/my_text_field.dart';
+import 'package:contacts_app/data/app_storage.dart';
 import 'package:flutter/material.dart';
 
-class AddPage extends StatelessWidget {
+class AddPage extends StatefulWidget {
   const AddPage({super.key});
+
+  @override
+  State<AddPage> createState() => _AddPageState();
+}
+
+class _AddPageState extends State<AddPage> {
+  final contactName = TextEditingController();
+  final contactPhone = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple[200],
-      appBar: AppBar(
-        title: const Padding(
-          padding: EdgeInsets.only(left: 16),
-          child: Text('Add Contact', style: TextStyle(
-            color: Colors.white,
-            fontSize: 24,
-            fontWeight: FontWeight.bold
-          ),),
+        backgroundColor: Colors.deepPurple[200],
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          title: const Text(
+            'Add Contact',
+            style: TextStyle(
+                color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          elevation: 0,
         ),
-        elevation: 0,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
-            child: MyTextField(hintText: 'Name', isText: true),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
-            child: MyTextField(hintText: 'Phone', isText: false),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Add Contact'),
-            style: ElevatedButton.styleFrom(
-              padding: const EdgeInsets.only(left: 20, right: 20),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10)
-              )
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+              child: MyTextField(
+                  hintText: 'Name', isText: true, controller: contactName),
             ),
-          )
-      ],)
-    );
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+              child: MyTextField(
+                  hintText: 'Phone', isText: false, controller: contactPhone),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                AppStorage.addData(contactName.text, contactPhone.text);
+                Navigator.pop(context);
+              },
+              style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10))),
+              child: const Text('Add Contact'),
+            )
+          ],
+        ));
   }
 }
